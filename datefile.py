766d4e0ce403ac2481b03aef34e73140c9a30385
Leanne Tai <leannecwb@gmail.com>
2021-05-02 21:28:14 +0800

    check 4 for datefiles

--- datefile.py
+++ datefile.py
@@ -144,14 +144,31 @@ def read_max_data_T(item,lines):
 		maxvalue = lines[30][5:9]
 		maxvalueT = lines[31][5:9]
 		datamax = [maxvalue,maxvalueT]
-		return datamax
+	elif item == 'P1Max':
+		maxvalue = lines[30][10:14]
+		maxvalueT = lines[31][10:14]
+		datamax = [maxvalue,maxvalueT]
 	else :
 		print('not right!')
 	return datamax	
 
-def main():
+def read_min_data_T(item,lines):
+	datamax=[]
+	if item == 'P2Min':
+		minvalue = lines[33][5:9]
+		minvalueT = lines[34][5:9]
+		datamin = [minvalue,minvalueT]
+	elif item == 'P1Min':
+		minvalue = lines[33][10:14]
+		minvalueT = lines[34][10:14]
+		datamin = [minvalue,minvalueT]
+	else :
+		print('not right!')
+	return datamin
 
-	lines = read_file('4670520210330_ACOS.txt')
+def main():
+	inputfile = input('請輸入日期檔:')
+	lines = read_file(inputfile)
 #	for i in range(2,27):
 #		print(lines[i])
 #		times.append(lines[i][0:4])
@@ -164,19 +181,102 @@ def main():
 #	print('max value ...')
 #	P2Max = read_max_data_T('P2Max',lines)
 #	print(P2Max[1])
-	print('檢查日期檔開始...')
-	print('++++++++   1. 檢查P2值的極值時間是否合理   +++++++')
+	print('\n\n檢查日期檔開始...\n\n')
+	print('=============================================================================\n')
+	print('+++++++++++++++   1. 檢查P2值的極值時間是否合理   ++++++++++++++')
 	P2 = read_data_h('P2',lines)
-	print(P2)
+#	print(P2)
 	p2_findmax = max(P2)
 	loc = P2.index(max(P2))
-	print(times[loc])
+	p2_findmin = min(P2)
+	loc2 = P2.index(min(P2))
+#	print('TIMES:',times[loc2])
 	P2Max = read_max_data_T('P2Max',lines)
-	print(P2Max)
+	P2Min = read_min_data_T('P2Min',lines)
+	#print(P2Min)
 	# check if P2Max >= p2_findmax
 	if float(P2Max[0]) >= float(p2_findmax) :
-		print('P2日極值 >= P2小時最大值，檢查通過...')
+		print('     o  P2日極大值(',P2Max[0],') >= P2小時最大值(',p2_findmax,')，檢查通過...')
 	else :
-		print('P2日極值(',P2Max,') < P2小時最大值('.p2_findmax,')，數值有問題請進行確認!!!')
+		print('-------------------------------------------------------------------------------------')	
+		print('     !!! P2日極大值(',P2Max[0],') < P2小時最大值(',p2_findmax,')，數值有問題請進行確認!!!')
+		print('--------------------------------------------------------------------------------------')
+
+
+	if  float(P2Max[1]) >= float(times[loc-1]) and  float(P2Max[1]) <= float(times[loc+1]):
+		print('     o  P2日極大值時間(',P2Max[1],') 接近P2小時最大值時間(',times[loc],')，檢查通過...')
+	else:
+		print('------------------------------------------------------------------------------------')	
+		print('     !!! P2日極大值時間(',P2Max[1],') 離P2小時最大值時間(',times[loc],')稍遠，請檢查!!!')
+		print('------------------------------------------------------------------------------------')
+#   P2 日極小值檢查
+	if float(P2Min[0]) <= float(p2_findmin) :
+		print('     o  P2日極小值(',P2Min[0],') <= P2小時最小值(',p2_findmin,')，檢查通過...')
+	else :
+		print('-------------------------------------------------------------------------------------')	
+		print('     !!! P2日極小值(',P2Min[0],') < P2小時最小值(',p2_findmin,')，數值可能有問題請進行確認!!!')
+		print('--------------------------------------------------------------------------------------')
+
+
+	if  float(P2Min[1]) >= float(times[loc2-1]) and  float(P2Min[1]) <= float(times[loc2+1]):
+		print('     o  P2日極小值時間(',P2Min[1],') 接近P2小時最小值時間(',times[loc2],')，檢查通過...')
+	else:
+		print('------------------------------------------------------------------------------------')	
+		print('     !!! P2日極小值時間(',P2Min[1],') 離P2小時最小值時間(',times[loc2],')稍遠，請檢查!!!')
+		print('------------------------------------------------------------------------------------')
+
+	print('+++++++++++++++   2. 檢查P1值的極值和時間是否合理   ++++++++++++++')
+	P1 = read_data_h('P1',lines)
+	p1_findmax = max(P1)
+	loc = P1.index(max(P1))
+	p1_findmin = min(P1)
+	loc1 = P1.index(min(P1))
+#	print('TIMES:',times[loc2])
+	P1Max = read_max_data_T('P1Max',lines)
+	P1Min = read_min_data_T('P1Min',lines)
+	#print(P1Min)
+	# check if P1Max >= p1_findmax
+	if float(P1Max[0]) >= float(p1_findmax) :
+		print('     o  P1日極大值(',P1Max[0],') >= P1小時最大值(',p1_findmax,')，檢查通過...')
+	else :
+		print('-------------------------------------------------------------------------------------')	
+		print('     !!! P1日極大值(',P1Max[0],') < P1小時最大值(',p1_findmax,')，數值有問題請進行確認!!!')
+		print('--------------------------------------------------------------------------------------')
+
+
+	if  float(P1Max[1]) >= float(times[loc-1]) and  float(P1Max[1]) <= float(times[loc+1]):
+		print('     o  P1日極大值時間(',P1Max[1],') 接近P1小時最大值時間(',times[loc],')，檢查通過...')
+	else:
+		print('------------------------------------------------------------------------------------')	
+		print('     !!! P1日極大值時間(',P1Max[1],') 離P1小時最大值時間(',times[loc],')稍遠，請檢查!!!')
+		print('------------------------------------------------------------------------------------')
+#   P1 日極小值檢查
+	if float(P1Min[0]) <= float(p1_findmin) :
+		print('     o  P1日極小值(',P1Min[0],') <= P1小時最小值(',p1_findmin,')，檢查通過...')
+	else :
+		print('-------------------------------------------------------------------------------------')	
+		print('     !!! P1日極小值(',P1Min[0],') < P1小時最小值(',p1_findmin,')，數值可能有問題請進行確認!!!')
+		print('--------------------------------------------------------------------------------------')
+
+
+	if  float(P1Min[1]) >= float(times[loc2-1]) and  float(P1Min[1]) <= float(times[loc2+1]):
+		print('     o  P1日極小值時間(',P1Min[1],') 接近P1小時最小值時間(',times[loc2],')，檢查通過...')
+	else:
+		print('------------------------------------------------------------------------------------')	
+		print('     !!! P1日極小值時間(',P1Min[1],') 離P1小時最小值時間(',times[loc2],')稍遠，請檢查!!!')
+		print('------------------------------------------------------------------------------------')
+	print('+++++++++++++++   3. 檢查P1的值是否皆小於P2   ++++++++++++++')
+	j = 0
+	for i in range(0,24):
+		if float(P1[i]) < float(P2[i]) :
+			continue
+		else:
+			print('------------------------------------------------------------------------------------')	
+			print('     !!! P1',P1[i],'>= P2',P2[i],'at TIME:',times[i],'請檢查!!!')
+			print('------------------------------------------------------------------------------------')
+			j = j + 1
+	if j == 0 :
+		print('     o  測站氣壓P1值皆小於海平面氣壓P2,檢查通過...')
+	print('+++++++++++++++   4. 檢查T的極值與時間是否合理   ++++++++++++++')
 
 main()
